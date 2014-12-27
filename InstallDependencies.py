4af13a3c54250f8d1df796a1c380234b0e70809e
Vale <valetolpegin@gmail.com>
2014-12-27 14:36:36 -0500

    Updated dependency installer
    Made it dynamically install the dependencies ( if a dependency is
    already installed, the program will not install it )

--- InstallDependencies.py
+++ InstallDependencies.py
@@ -16,66 +16,92 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- 
- 
+
 import os
 
 #This method will install and test the dependencies
 def install():
+    #Installing each dependency that is not already installed ( we do not want duplicates or any errors to occur because of this )
+    
+    print ""
     print "Beginning installation....."
     print ""
     
-    #Installing CherryPy
+    #------------------------------------------------------------------------------------
+    #Installing CherryPy and testing to see if the installation was successful
+    
     os.chdir( "dependencies/CherryPy/" )
-    os.system( "python setup.py install" )
-    
-    print ""
-    print "CherryPy installation complete"
-    print ""
-    
-    #Installing PySerial
-    os.chdir( "../pyserial" )
-    os.system( "python setup.py install" )
 
-    print ""
-    print "PySerial installation complete"
-    print ""
-    
-    #Installing ws4py
-    os.chdir( "../ws4py" )
-    os.system( "python setup.py install" )
-    
-    print ""
-    print "ws4py installation complete"
-    print ""
-
-    #--------------------------------------------------------------------------------------
-    
-    #Testing whether installation was successful for each dependency
-
-    #Testing whether CherryPy was installed correctly
     version = os.system( 'python -c "import cherrypy;print cherrypy.__version__"' )
     if ( version != 0 ):
+        os.system( "python setup.py install" )
+
         print ""
-        print "ERROR......INSTALLATION OF CherryPy HAS FAILED"
+        print "CherryPy installation complete"
         print ""
-        exit()
+
+        version = os.system( 'python -c "import cherrypy;print cherrypy.__version__"' )
+        if ( version != 0 ):
+            print ""
+            print "ERROR......INSTALLATION OF CherryPy HAS FAILED"
+            print ""
+            exit()
+    else:
+        print ""
+        print "CherryPy already installed.....moving on to next installation"
+        print ""
+    
+    #------------------------------------------------------------------------------------
+    #Installing PySerial and testing to see if the installation was successful
+    
+    os.chdir( "../pyserial" )
 
     version = os.system( 'python -c "import serial;print serial.Serial()"' )
     if ( version != 0 ):
+        os.system( "python setup.py install" )
+
         print ""
-        print "ERROR......INSTALLATION OF PySerial HAS FAILED"
+        print "PySerial installation complete"
         print ""
-        exit()
+
+        version = os.system( 'python -c "import serial;print serial.Serial()"' )
+        if ( version != 0 ):
+            print ""
+            print "ERROR......INSTALLATION OF PySerial HAS FAILED"
+            print ""
+            exit()
+    else:
+        print ""
+        print "PySerial already installed.....moving on to next installation"
+        print ""
+
+    #------------------------------------------------------------------------------------
+    #Installing ws4py and testing to see if the installation was successful
+    
+    os.chdir( "../ws4py" )
 
     version = os.system( 'python -c "import ws4py;print ws4py.__version__"' )
     if ( version != 0 ):
+        os.system( "python setup.py install" )
+    
         print ""
-        print "ERROR......INSTALLATION OF ws4py HAS FAILED"
+        print "ws4py installation complete"
         print ""
-        exit()
 
-    print ""
+        version = os.system( 'python -c "import ws4py;print ws4py.__version__"' )
+        if ( version != 0 ):
+            print ""
+            print "ERROR......INSTALLATION OF ws4py HAS FAILED"
+            print ""
+            exit()
+    else:
+        print ""
+        print "ws4py already installed.....moving on to next installation"
+        print ""
+
+    #------------------------------------------------------------------------------------
+    #If all of the installations were successful, telling user installation was successful
+
     print "Installation was successful....."
     print ""
 
