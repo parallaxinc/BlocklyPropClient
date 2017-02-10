754cefd36a4c4eadd928228e2adffb0392f9e53e
Jim Ewald <jewald@parallax.com>
2017-02-10 09:30:23 -0800

    Add stub code for macos support

--- BlocklyLogger.py
+++ BlocklyLogger.py
@@ -1,7 +1,9 @@
+import os
 import logging
 from sys import platform
 from subprocess import Popen, PIPE, check_output, CalledProcessError
 
+# Define error constants
 
 # Logging path
 try:  # Python 2.7+
@@ -39,12 +41,22 @@ logger.addHandler(console)
 
 def init():
     logger.info("Logger has been started.")
+
+    # Debian linux
     if platform == 'linux2':
         if is_module_loaded() != 0:
             return
 
         if check_system_messages() != 0:
             return
+
+     # MacOS
+    if platform == 'darwin':
+        if is_macos_ftdi_module_installed() != 0:
+            return 101
+
+        if is_macos_ftdi_module_loaded != 0:
+            return 102
 
 
 def is_module_loaded():
@@ -69,3 +81,39 @@ def check_system_messages():
     except CalledProcessError:
         logger.warning('Serial port is not assigned.')
         return 1
+
+
+def check_macos_logpath():
+    user_home = os.path.expanduser('~')
+    log_path = user_home + '/Library/Logs/BlocklyProp'
+
+    # Does the log directory exist
+    try:
+        os.stat(log_path)
+        return 0
+
+    except OSError:
+        # Create a new log path
+        try:
+            os.mkdirs(log_path)
+
+        except OSError:
+            return 1
+
+    return 0
+
+
+def is_macos_ftdi_module_loaded():
+    try:
+        process = Popen(['kextstat'], stdout=PIPE, stderr=PIPE)
+        output = check_output(('grep', 'FTDI'), stdin=process.stdout)
+        logger.debug('FTDI module load state')
+        logger.debug(output)
+        return 0
+    except CalledProcessError:
+        logger.warning('No FTDI modules detected.')
+        return 1
+
+
+def is_macos_ftdi_module_installed():
+    return 0
