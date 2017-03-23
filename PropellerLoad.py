055bf4e52b57e921b900fd8ceb1541ac26f50bd8
Developer <developer@blocklydev.local>
2017-03-22 18:28:15 -0700

    More attempts to finish download function.  Doesn't work.

--- PropellerLoad.py
+++ PropellerLoad.py
@@ -94,20 +94,20 @@ class PropellerLoad:
                 self.appdir = os.path.dirname(os.path.realpath(sys.argv[0]))
 
         # Set command download to RAM or EEPROM and run afterwards
-        command = self.loaderAction[action]["compile-options"] + "-r"
+        command = [self.loaderAction[action]["compile-options"], "-r"]
 
         # Add requested port
         if com_port is not None:
             targetWiFi = [l for l in self.wports if isWiFiName(l, com_port)]
             if len(targetWiFi) == 1:
                 self.logger.debug('Requested port %s is at %s', com_port, getWiFiIP(targetWiFi[0]))            
-                command += " -i " + getWiFiIP(targetWiFi[0]).encode('ascii', 'ignore')
+                command += ["-i", getWiFiIP(targetWiFi[0]).encode('ascii', 'ignore')]
             else:
                 self.logger.debug('Requested port is %s', com_port)
-                command += " -p " + com_port.encode('ascii', 'ignore')
+                command += ["-p", com_port.encode('ascii', 'ignore')]
 
         # Add target file
-        command += ' ' + file_to_load.name.encode('ascii', 'ignore').replace('\\', '/')
+        command += [file_to_load.name.encode('ascii', 'ignore').replace('\\', '/')]
 
         # Download
         success, out, err = loader(self, command)
@@ -119,14 +119,14 @@ class PropellerLoad:
 def loader(self, cmdOptions):
     # Launch Propeller Loader with cmdOptions and return True/False, output and error string
     try:
-        self.logger.debug('Loader command: %s', self.appdir + self.loaderExe[platform.system()] + ' ' + cmdOptions)
+        self.logger.debug('Loader command: %s', self.appdir + self.loaderExe[platform.system()] + ' ' + str(cmdOptions))
 
         if platform.system() == "Windows":
             startupinfo = subprocess.STARTUPINFO()
             startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-            process = subprocess.Popen([self.appdir + self.loaderExe[platform.system()], cmdOptions], stdout=subprocess.PIPE, stderr=subprocess.PIPE, startupinfo=startupinfo)
+            process = subprocess.Popen([self.appdir + self.loaderExe[platform.system()], [cmdOptions]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, startupinfo=startupinfo)
         else:
-            process = subprocess.Popen([self.appdir + self.loaderExe[platform.system()], cmdOptions], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process = subprocess.Popen([self.appdir + self.loaderExe[platform.system()], [cmdOptions]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         out, err = process.communicate()
 
