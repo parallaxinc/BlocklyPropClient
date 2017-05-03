a7ce8c0c252ee2af8dfce2e3c1aca5e19045de10
Developer <developer@blocklydev.local>
2017-05-03 10:32:21 -0700

    Sorted wired ports alphanumerically, but kept wired ports at top of list (wireless ports at bottom).

--- PropellerLoad.py
+++ PropellerLoad.py
@@ -63,6 +63,7 @@ class PropellerLoad:
         (success, out, err) = loader(self, ["-P"])
         if success:
             self.ports = out.splitlines()
+            self.ports.sort(None, None, False)
         else:
             self.logger.debug('COM Port request returned %s', err)
  
